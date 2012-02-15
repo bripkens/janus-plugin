b760130ec90d321f2c410fa258dc51b22e551cff
Ben Ripkens <bripkens.dev@gmail.com>
2012-02-15 14:02:53 +0100

    Managed to get a the configuration menu working.

--- src/main/java/de/codecentric/janus/plugin/Root.java
+++ src/main/java/de/codecentric/janus/plugin/Root.java
@@ -2,8 +2,13 @@ package de.codecentric.janus.plugin;
 
 import de.codecentric.janus.scaffold.Catalog;
 import hudson.Extension;
+import hudson.model.Describable;
+import hudson.model.Descriptor;
 import hudson.model.RootAction;
+import net.sf.json.JSONObject;
+import org.kohsuke.stapler.StaplerRequest;
 
+import javax.management.DescriptorAccess;
 import java.util.Arrays;
 import java.util.List;
 
@@ -11,7 +16,7 @@ import java.util.List;
  * @author Ben Ripkens <bripkens.dev@gmail.com>
  */
 @Extension
-public class Root implements RootAction{
+public class Root implements RootAction, Describable<Root>{
     public String getIconFileName() {
         return "new-package.png";
     }
@@ -23,11 +28,41 @@ public class Root implements RootAction{
     public String getUrlName() {
         return "new-project";
     }
-    
+
+    public DescriptorImpl getDescriptor() {
+        return new DescriptorImpl();
+    }
+
     public List<String> getScaffolds() {
         // TODO receive list of scaffolds from file system
         return Arrays.asList("Spring/Vadin web app",
                 "Batch file processing", "Single page web app",
                 "Transaction aware system");
+    }
+
+    @Extension
+    public static final class DescriptorImpl extends Descriptor<Root> {
+        private String scaffoldDirectory;
+
+        public DescriptorImpl() {
+            load();
+        }
+
+        @Override
+        public String getDisplayName() {
+            return "Janus configuration";
+        }
+
+        @Override
+        public boolean configure(StaplerRequest req, JSONObject formData)
+                throws FormException {
+            scaffoldDirectory = formData.getString("scaffoldDirectory");
+            save();
+            return super.configure(req,formData);
+        }
+
+        public String getScaffoldDirectory() {
+            return scaffoldDirectory;
+        }
     }
 }
